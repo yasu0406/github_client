4c9c600741e3e9b59ffc91b78fbc07ec123b5477
katayama <yasucom4646@gmail.com>
2018-12-20 08:58:14 -0800

    Added Detail

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import {createStackNavigator, createAppContainer, NavigationActions, StackActions,} from 'react-navigation';
 import Home from './Home';
+import Detail from './Detail'
 const MainNavigator = createStackNavigator(
     {
-        Home: {screen: Home},
-        navigationOptions: () => ({
-            title: `Home`,
-            headerBackTitle: null
-        }),
+        Home: {
+            screen: Home,
+            navigationOptions: {
+                title: `Home`,
+            }
+        },
+        Detail: {
+            screen: Detail,
+            navigationOptions: ({ navigation }) => ({
+                title: navigation.state.params.item.name,
+            })
+        }
+    },{
+        initialRouteName: 'Home',
     });
 const App = createAppContainer(MainNavigator);
 export default App;
