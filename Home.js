07c72cb4b2576c24e6a57b23eeaf6d90be20c93e
katayama <yasucom4646@gmail.com>
2018-12-20 08:58:57 -0800

    Created Search function

--- Home.js
+++ Home.js
@@ -7,18 +7,20 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList, TextInput, Image, AppState} from 'react-native';
 
 export default class Home extends Component {
     state = {
         items: [],
         refreshing: false,
+        text: '',
     }
     page = 0;
     fetchRepositories(refreshing = false) {
         const newPage = refreshing ? 1 : this.page + 1;
         this.setState({ refreshing });
-        fetch('https://api.github.com/search/repositories?q=react&page=${newPage}')
+        console.log(this.state.text);
+        fetch(`https://api.github.com/search/repositories?q=${this.state.text}&page=${newPage}`)
             .then(response => response.json())
             .then(({ items }) => {
                 this.page = newPage;
@@ -30,15 +32,44 @@ export default class Home extends Component {
             });
     }
 
+    navigateToDetail(item) {
+        this.props.navigation.navigate('Detail', { item });
+    }
+
+    componentDidMount() {
+        AppState.addEventListener('change', this.onChangeState);
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this.onChangeState);
+    }
+
+    onChangeState = (appState) => {
+        if(appState === 'active') {
+            this.fetchRepositories(true);
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={{marginTop: 20}} onPress={() => this.fetchRepositories()}>
-                    <Text>Fetch</Text>
-                </TouchableOpacity>
+                <View style={styles.inputWrapper}>
+                    <TextInput style={styles.input} onChangeText={(text) => this.setState({ text })}/>
+                    <TouchableOpacity onPress={() => this.fetchRepositories(true)}>
+                        <Text style={styles.serachText}>Search</Text>
+                    </TouchableOpacity>
+                </View>
                 <FlatList
                     data={this.state.items}
-                    renderItem={({item}) => <Text style={{padding: 20}}>{item.name}</Text>}
+                    renderItem={({item}) =>
+                        <TouchableOpacity  style={{padding:10}} onPress={() => this.navigateToDetail(item) }>
+                            <Text style={{fontSize: 20, fontWeight:'bold', marginBottom:10}}>{item.name}</Text>
+                            <View style={{flexDirection: 'row'}}>
+                                <Image style={ styles.ownerIcon } source={{uri: item.owner.avatar_url}}/>
+                                <Text style={ styles.ownerName }>{item.owner.login}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
                     keyExtractor={(item) => item.id}
                     onEndReached={() => this.fetchRepositories()}
                     onEndReachedThreshold={0.1}
@@ -53,8 +84,30 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    inputWrapper: {
+        padding: 20,
+        flexDirection: 'row',
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+    },
+    input: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#EEE',
+        borderRadius: 4,
+    },
+    serachText: {
+        padding: 10,
+    },
+    ownerIcon: {
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        marginRight: 5,
+    },
+    ownerName: {
+        fontSize: 14
     },
 });
